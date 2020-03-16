b1879e80b8af649aab32dbd3b12fac6008a05f3b
Joshen Lim <joshenlimek@gmail.com>
2020-03-17 03:48:31 +0800

    Built Flask Server for CRNN model along with API endpoint

---
+++ server/endpoints/image_compute.py
@@ -0,0 +1,71 @@
+from flask import request, render_template
+from flask_restful import Resource, Api, reqparse
+from PIL import Image
+from io import BytesIO
+import base64
+import numpy as np
+import cv2
+from utils.cv2_helper import angular_correction
+
+parser = reqparse.RequestParser()
+
+'''
+body = {
+  image: base64encoded-image
+}
+'''
+
+class ImageCompute(Resource):
+    def __init__(self, detector, recognizer):
+        self.detector = detector
+        self.recognizer = recognizer
+
+    def post(self):
+        parser.add_argument('image')
+        args = parser.parse_args()
+        img_b64 = args['image']
+        image = Image.open(BytesIO(base64.b64decode(img_b64))).convert('RGB')
+        image_cv = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+
+        boxes, confidences, indices, width_ratio, height_ratio = self.detector.detect(image_cv)
+        index_map = {}
+
+        for i in indices:
+            key = i[0]
+            vertices = cv2.boxPoints(boxes[i[0]])
+            for j in range(4):
+                vertices[j][0] *= width_ratio
+                vertices[j][1] *= height_ratio
+
+            top_left = (min([vertices[0][0], vertices[1][0]]), min([vertices[1][1], vertices[2][1]]))
+            btm_right = (max([vertices[2][0], vertices[3][0]]), max([vertices[0][1], vertices[3][1]]))
+
+            if top_left[0] < 0 or top_left[1] < 0:
+                continue
+
+            text_roi = angular_correction(key, image_cv, vertices)
+
+            if text_roi.shape[0] > 0 and text_roi.shape[1] > 0:
+                index_map[key] = {
+                  'vertices': vertices,
+                  'pred_text': self.recognizer.predict(text_roi),
+                }
+
+        if len(index_map.keys()):
+            try:
+                for i in indices:
+                    key = i[0]
+                    for j in range(4):
+                        p1 = (index_map[key]['vertices'][j][0], index_map[key]['vertices'][j][1])
+                        p2 = (index_map[key]['vertices'][(j + 1) % 4][0], index_map[key]['vertices'][(j + 1) % 4][1])
+                        cv2.line(image_cv, p1, p2, (255, 170, 0), 2, cv2.LINE_AA)
+                    
+                    cv2.putText(image_cv, index_map[key]['pred_text'], (index_map[key]['vertices'][1][0], index_map[key]['vertices'][1][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
+            except:
+                pass
+
+        img_str = base64.b64encode(cv2.imencode('.jpg', image_cv)[1]).decode()
+
+        return {
+          'image': img_str
+        }
---
+++ server/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from flask_restful import Resource, Api
+import requests
+import numpy as np
+import base64
+
+from server.endpoints.image_compute import ImageCompute
+
+from east_text_detector.detector import EASTDetector
+from crnn_text_recognizer.recognizer import CRNNRecognizer
+
+east_model_path = './east_text_detector/frozen_east_text_detection.pb'
+crnn_model_path = './crnn_text_recognizer/crnn_text_recognizer_best.pth'
+demo_image_path = './assets'
+white_list = ['.DS_Store']
+
+text_detector = EASTDetector(east_model_path)
+text_recognizer = CRNNRecognizer(crnn_model_path)
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(ImageCompute, '/compute', resource_class_kwargs={'detector': text_detector, 'recognizer': text_recognizer})
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        image = request.files.get('image')
+        img_b64 = base64.b64encode(image.read()).decode('ascii')
+
+        res = requests.post('http://localhost:5000/compute', data={
+          'image': img_b64
+        })
+
+        computed_img = base64.b64decode(res.json()['image'])
+        computed_img_b64 = base64.b64encode(computed_img).decode('ascii')
+
+        return render_template('home.html', image=f'data:image/png;base64,{computed_img_b64}')
+    else:
+        return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # app.run()
